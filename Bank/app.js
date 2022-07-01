87163c2986d2eda80038fbf090789b4deffa7448
JuanPabloVonLandwust <JuanPablo.vonLandwust@jala-foundation.org>
2022-06-30 20:58:21 -0400

    Added lots of dialog functions to the site

--- Bank/app.js
+++ Bank/app.js
@@ -1,1 +1,7 @@
+alert(10 / 3);
+document.write(prompt("Enter the student ID:", "00000000"));
+if (confirm("Leave the site?"))
+    console.log(10 / 3);
+else
+    console.log("Not leaving");
 console.log("Welcome to our bank!");
