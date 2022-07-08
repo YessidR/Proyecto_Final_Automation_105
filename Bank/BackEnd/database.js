56323267a1005ad96d7a912e9a94356e29062196
JuanPabloVonLandwust <JuanPablo.vonLandwust@jala-foundation.org>
2022-07-08 10:24:09 -0400

    Added database connection script & dependencies

---
+++ Bank/BackEnd/database.js
@@ -0,0 +1,14 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const DB_PATH = "./databases/bank.db";
+
+const db = new sqlite3.Database(DB_PATH, (err) => {
+    if (err) {
+        console.error(err.message);
+        throw err;
+    }
+    else
+        console.log("Connected to the SQLite database.");
+});
+
+module.exports = db;
