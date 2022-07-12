80e1d0780887e1491282a014284c7932528651e0
gmgandon <guidomgandon@gmail.com>
2022-07-12 17:02:23 -0300

    add functionality to populate courses tab with list of pulled subjects

--- University/UI/enrollment.js
+++ University/UI/enrollment.js
@@ -1,5 +1,7 @@
 //list of codes pulled from db
 const codes = ["AAAAA", "BBBBB", "CCCCC", "DDDDD", "EEEEE"]
+let courses = ["Math", "Algebra", "Trigonometry", "Calculus", "Something else", "Computer science"]
+
 
 //this boolean will be pulled from student class
 let userEnrolled = false
@@ -11,9 +13,24 @@ let modalElement = document.querySelector(".modal-text");
 const modalContainer = document.querySelector(".modal-container");
 const modalCloseButton = document.querySelector(".modal-close");
 const checkBoxes = document.getElementsByName("checkbox");
+const endEndrollmentButton = document.querySelector(".end-enrollment")
+
+
 
 checkCodeHandler.addEventListener('click', checkCodes)
 modalCloseButton.addEventListener('click', hideModal)
+endEndrollmentButton.addEventListener('click', setSubjects)
+
+
+const subjectContainer = document.querySelector(".subject-container")
+courses.forEach(course => {
+    
+    var newDiv = document.createElement("div");
+    newDiv.className = 'subject';
+    newDiv.innerHTML = (`${course} <input class="subject-checkbox" type="checkbox" name="checkbox" id="checksubject"></div>`);
+
+    subjectContainer.appendChild(newDiv)
+});
 
 
 function hideModal() {
@@ -81,8 +98,26 @@ const maxSubjects = 5;
 for (var i = 0; i < checkBoxes.length; i++)
   checkBoxes[i].onclick = selectiveCheck;
 
-function selectiveCheck (event) {
+function selectiveCheck () {
   let checkedChecks = document.querySelectorAll(".subject-checkbox:checked");
   if (checkedChecks.length >= maxSubjects + 1)
     return false;
+}
+
+
+function setSubjects () {
+    let checkedChecks = document.querySelectorAll(".subject-checkbox:checked");
+    if (checkedChecks.length > 0) {
+        enrolledsubjects = []
+        checkedChecks.forEach(checkbox => {
+            let checkedSubjectElem =checkbox.parentElement
+
+            enrolledsubjects.push(checkedSubjectElem.textContent)
+
+        });
+        alert(`You have successfully enrolled in ${enrolledsubjects.join(". ")}`)
+    } else {
+        alert("No subjects selected!")
+    }
+    return enrolledsubjects
 }
