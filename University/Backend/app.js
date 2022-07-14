a4a018b97a3e189ea171f64c75e723e6717e3a96
DPardoJalasoft <david.pardo@jala-foundation.org>
2022-07-14 15:21:12 -0400

    databaseManager.js

--- University/Backend/app.js
+++ University/Backend/app.js
@@ -2,25 +2,12 @@
 const express = require("express");
 const app = express();
 const enrollmentRouter = require("./routes/student");
-const createDBQuery = require('./config/databaseManager')
 // Server port
 const HTTP_PORT = 8000;
-
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log(`Server running on port ${HTTP_PORT}`)
 });
-
-// Create Enrollment DB
-db.run(createDBQuery, err => {
-	if (err) {
-		return console.error(err.message);
-	}else{
-		console.log("Table Enrollment created successfully")
-        console.log("The route is: " + db_name)
-        console.log("")
-	}
-})
 
 app.use("/enrollment", enrollmentRouter);
 
