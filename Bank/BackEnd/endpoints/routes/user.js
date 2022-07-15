1375a0413d6dc9753300dd88cc946170ebcc5f55
JuanPabloVonLandwust <JuanPablo.vonLandwust@jala-foundation.org>
2022-07-14 20:34:30 -0400

    Moved some scripts to proper folders

--- Bank/BackEnd/endpoints/routes/user.js
+++ Bank/BackEnd/endpoints/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router= express.Router();
-const db = require("../database.js");
+const db = require("../../database.js");
 const login={
     user:"",
     password:"",
