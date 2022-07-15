c1e9e4e139b85ea3b0da83ec4ef9c594817c32ae
QuantumGorilla <manotasja@uninorte.edu.co>
2022-07-15 16:49:07 -0500

    Delete update from studentEnrollment controller

--- University/Backend/routes/studentEnrollment.js
+++ University/Backend/routes/studentEnrollment.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { studentEnrollment, 
     getEnrollment, 
-    updateStudents, 
     deleteStudents,
     getAllStudentEnrollments } = require('../controllers/studentEnrollment');
 
