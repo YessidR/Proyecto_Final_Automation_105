5c7bb2a2c518910e81ab4c00e19a545f4fee3274
Yessid Rios <91134063+YessidR@users.noreply.github.com>
2022-07-18 10:10:28 -0500

    Test n...

--- University/Backend/app.js
+++ University/Backend/app.js
@@ -1,27 +1,6 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path')
-
-// Configurar servidor app
-app.engine('html', require('ejs').renderFile);
-app.set("view engine", "html");
-app.set("views", path.join(__dirname, ""));
-app.use(express.urlencoded({extended:false}));
-
-const enrollmentRouter = require("./routes/student");
-
-
-const { createTables } = require('./config/databaseManager')
-
-createTables()
-
-// // Server port
-const HTTP_PORT = 8001;
-
-=======
 const enrollmentRouter = require("./routes/studentEnrollment");
 const cors=require("cors");
 const corsOptions ={
@@ -32,31 +11,12 @@ const corsOptions ={
 
 // Server port
 const HTTP_PORT = 8000;
->>>>>>> 8c9e03b71622154686d8e3b4bd94ac6058a8c3a9
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log(`Server running on port ${HTTP_PORT}`)
 });
 
-<<<<<<< HEAD
-app.use("/enrollment", enrollmentRouter); //
-
-
-// console.log(`la direccion es ${path.join(__dirname)}`)
-// app.use(express.static(__dirname));
-// // app.use(express.static(__dirname+"../UI/html"));
-
-
-// app.get('/asd'), (req, res) => {
-//     res.render('../UI/html/index')
-// }
-
-// app.get('/'), (req, res) => {
-//     res.render('index.html')
-// }
-=======
 app.use(cors(corsOptions))
 app.use("/enrollment", enrollmentRouter);
->>>>>>> 8c9e03b71622154686d8e3b4bd94ac6058a8c3a9
 
 
--- University/Backend/config/dataEnrollment.js
+++ University/Backend/config/dataEnrollment.js
@@ -1,28 +1,32 @@
+const { database } = require('./databaseManager');
+
 // Open csv files...
 const path = require('path') // Genera proporciona ubicacion
 const CSV = require('csv-parser')
 const fs = require('fs') // File stream
 const results = []; // Store data from csv
 
-const CSVSOURCE = path.resolve('./University/Backend/config/Test-Enrollment.csv'); // csv file location
-console.log (`location is: ${CSVSOURCE}`) //To confirm the location
+const CSVSOURCE = path.resolve('./University/Backend/config/Test-enrollment.csv'); // csv file location
+// console.log (`location is: ${CSVSOURCE}`) //To confirm the location
 
-string = 'abcdefghijklmnopqrstuvwxyz'
-for (let i = 0; i < string.length; i++) {
-    console.log(string[i])
-}
+// Creacion de codigos....
+// string = 'abcdefghijklmnopqrstuvwxyz'
+// for (let i = 0; i < string.length; i++) {
+//     console.log(string[i])
+// }
 
 fs.createReadStream(CSVSOURCE)
 .pipe(CSV({}))
 .on('data', (data) => results.push(data))
 .on('end', () => {
-    console.log(results)
+    // console.log(results)
     for (let i = 0; i < results.length; i++) {
         let obj = results[i]
-        // console.log(obj.ID)
-        querySelect = `SELECT * FROM productos WHERE id_enrollment=${obj.id_enrollment}`
-        queryAdd = 
-        console.log(querySelect)
+        // console.log(`Objeto en posicion ${i}`, obj)
+        queryEnrollment = `INSERT INTO Enrollment (id_enrollment, validation_date, date) VALUES (${obj.id_enrollment}, ${obj.validation_date}, ${obj.date})`
+        console.log(queryEnrollment)
+        querySelect = `UPDATE INTO Student (status) WHERE id_enrollment = ${obje.id_enrollment} VALUES (status = True)`
+    //     // queryAdd = ""
     }
 });
 
