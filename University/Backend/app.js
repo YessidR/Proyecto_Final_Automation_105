0f367e9216749ceab3f9a6236dc4998031396ddd
GuilleAlvarado <guillermo.alvarado@jala-foundation.org>
2022-07-18 16:39:45 -0400

    update profile

--- University/Backend/app.js
+++ University/Backend/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const enrollmentRouter = require("./routes/studentEnrollment");
+const profileRouter = require("./routes/profile");
 const cors=require("cors");
 const corsOptions ={
    origin:'*', 
@@ -11,7 +12,7 @@ const corsOptions ={
 const bodyParser = require('body-parser');
 
 // Server port
-const HTTP_PORT = 8000;
+const HTTP_PORT = 8002;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log(`Server running on port ${HTTP_PORT}`)
@@ -21,6 +22,7 @@ app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/enrollment", enrollmentRouter);
+app.use("/profile", profileRouter);
 
 // Default response for any other request
 app.use(function(req, res){
