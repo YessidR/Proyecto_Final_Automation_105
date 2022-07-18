2097b4001a301da2790afce6c6ed2f852efdd34f
gmgandon <guidomgandon@gmail.com>
2022-07-18 18:59:20 -0300

    add id_student, lastname, firstname to localstorage

--- University/UI/scripts/login.js
+++ University/UI/scripts/login.js
@@ -2,7 +2,7 @@ let userElem = document.getElementById('user')
 let passwordElem = document.getElementById('password')
 let logInButton = document.getElementById('boton')
 let wrongPassOrUser = document.querySelector('.msg')
-let userId = ""
+
 
 logInButton.addEventListener('click', getCredentials, hideMsg)
 
@@ -20,16 +20,19 @@ function getCredentials(){
     fetch(`http://localhost:8000/auth/${userElem.value}`).then((res) =>{
         return res.json()
     }).then((data) => {
-        userId = data.data.userId
-        checkCredentials(data.data.password)
-    })  
+        let userId = (data.data.id_student)
+        let firstname = (data.data.first_name)
+        let lastname = (data.data.last_name)
+        localStorage.setItem('userId', userId)
+        localStorage.setItem('firstname', firstname)
+        localStorage.setItem('lastname', lastname)
+        checkCredentials(data.data.password);
+    })
 }
 
 function checkCredentials(pw) {
     if (passwordElem.value === pw) {
-        console.log('credentials ok')
         hideMsg()
-  
         window.location.replace('./index.html')
         return userId;
     } else {
