be0889ca7fe2222d8ab35a03c7ebd0835c55663c
QuantumGorilla <manotasja@uninorte.edu.co>
2022-07-22 13:30:44 -0500

    Tests added

---
+++ University/Backend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+  
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: [
+      '/node_modules/',
+    ],
+  
+    // Indicates whether each individual test should be reported during the run
+    verbose: true,
+  };
